f8078fd9637bee33ba232236ce873f7fdeed7a59
Николай Карнаев <karnaev.nick@gmail.com>
2021-05-06 23:41:48 +0300

    Отступы по краям страницы

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,25 +5,25 @@ const Footer = () => {
     <footer className="footer">
       <nav className="footer__nav">
         <ul className="footer__nav-list">
-          <li>
+          <li className="footer__nav-item">
             <a className="footer__nav-link" href="#">Корпоративным клиентам</a>
           </li>
-          <li>
+          <li className="footer__nav-item">
             <a className="footer__nav-link" href="#">Клиентам</a>
           </li>
-          <li>
+          <li className="footer__nav-item">
             <a className="footer__nav-link" href="#">Аренда авто</a>
           </li>
-          <li>
+          <li className="footer__nav-item">
             <a className="footer__nav-link" href="#">Каршеринг</a>
           </li>
-          <li>
+          <li className="footer__nav-item">
             <a className="footer__nav-link" href="#">Как продать авто</a>
           </li>
-          <li>
+          <li className="footer__nav-item">
             <a className="footer__nav-link" href="#">Trade-In</a>
           </li>
-          <li>
+          <li className="footer__nav-item">
             <a className="footer__nav-link" href="#">Test-drive</a>
           </li>
         </ul>
